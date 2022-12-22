ec528f7ae5ffe7de731a07c459beb4e6d4bc1060
Jacob <144292797+raducbd@users.noreply.github.com>
2022-12-22 09:56:49 -0300

    update Job and Task docstrings

--- scheduler/manufacturing/job.py
+++ scheduler/manufacturing/job.py
@@ -6,15 +6,22 @@ from scheduler.manufacturing.task import Task
 
 @dataclass
 class Job:
-    """Class for identifying a job and its sassociated tasks."""
+    """Class for identifying a job and its associated tasks.
+    
+    Args:
+        id (int): Job unique identifier
+        tasks (List[Task]): List of all tasks required for the job to run completely
+    """
 
     id: int
     tasks: List[Task]
 
     @property
     def horizon(self) -> int:
+        """(int) Job's horizon. Conceptually, it is the maximum job duration if all Tasks run sequentially."""
         return sum(task.duration for task in self.tasks)
 
     @property
     def machines(self) -> List[int]:
+        """(List[int]) All machines required for running job's tasks."""
         return list(set([task.machine for task in self.tasks]))
