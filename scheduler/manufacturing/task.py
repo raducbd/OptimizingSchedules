d8466bfe76439a6cf21d72082338a3597015fbbf
Jacob <144292797+raducbd@users.noreply.github.com>
2022-12-22 09:58:07 -0300

    update Task Docstring

--- scheduler/manufacturing/task.py
+++ scheduler/manufacturing/task.py
@@ -5,7 +5,16 @@ from ortools.sat.python import cp_model
 
 @dataclass
 class Task:
-    """Class for identifying a task in a given job."""
+    """Class for identifying a task in a given job.
+    
+    Args:
+        id (int): Task unique identifier
+        machine (int): Machine ID where task runs
+        duration (int): Task total duration
+        start (int, cp_model.IntVar): Task start time - optimization goal
+        end (int, cp_model.IntVar): Task end time - optimization goal
+        interval (int, cp_model.IntervalVar): Task interval - optimization goal
+    """
 
     id: int
     machine: int
