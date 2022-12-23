d57de8c78936002771600ee74a3445a96c965a71
Jacob <144292797+raducbd@users.noreply.github.com>
2022-12-23 18:15:35 -0300

    add unit tests for Scheduler class

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 """
 Module that contains objects used as inputs for test scenarios.
 """
+import pandas as pd
 import pytest
 
+from scheduler import Scheduler
 from scheduler.manufacturing import Task, Job
 
 
@@ -24,3 +26,64 @@ def job_example() -> Job:
         Job: Mocked job example for testing.
     """
     return Job(id=99, tasks=[Task(id=99, machine=20, duration=21) for _ in range(2)])
+
+
+@pytest.fixture(scope="module")
+def scheduler_example() -> Scheduler:
+    """Scheduler example for testing purposes.
+
+    Returns:
+        Scheduler: Mocked scheduler example for testing.
+    """
+    return Scheduler(
+        jobs=[
+            Job(
+                id=0,
+                tasks=[
+                    Task(id=0, machine=1, duration=2),
+                    Task(id=1, machine=2, duration=2),
+                    Task(id=2, machine=3, duration=2),
+                ],
+            ),
+            Job(
+                id=1,
+                tasks=[
+                    Task(id=0, machine=3, duration=2),
+                    Task(id=1, machine=2, duration=2),
+                    Task(id=2, machine=1, duration=2),
+                ],
+            ),
+        ]
+    )
+
+
+@pytest.fixture(scope="module")
+def results_example() -> pd.DataFrame:
+    """Scheduler example for testing purposes.
+
+    Returns:
+        Scheduler: Mocked scheduler example for testing.
+    """
+    return pd.DataFrame(
+        {
+            "job_id": [0, 1, 0, 1, 1, 0],
+            "id": [
+                "job(0, 0)",
+                "job(1, 2)",
+                "job(0, 1)",
+                "job(1, 1)",
+                "job(1, 0)",
+                "job(0, 2)",
+            ],
+            "machine": [
+                "Machine #1",
+                "Machine #1",
+                "Machine #2",
+                "Machine #2",
+                "Machine #3",
+                "Machine #3",
+            ],
+            "start": [0, 6, 2, 4, 0, 4],
+            "end": [2, 8, 4, 6, 2, 6],
+        }
+    )
---
+++ tests/test_scheduling.py
@@ -0,0 +1,78 @@
+from functools import reduce
+import pandas as pd
+import pytest
+
+from scheduler import Scheduler
+from scheduler.manufacturing import Job, Task
+from scheduler.utils.errors import ModelNotFitted
+
+
+class TestScheduler(object):
+    def test_SchedulerInstantiated(self, scheduler_example: Scheduler):
+        # OUTPUT
+        output = scheduler_example.__dict__
+
+        # EXPECTED
+        expected = {
+            "_jobs": {
+                0: Job(
+                    id=0,
+                    tasks=[
+                        Task(id=0, machine=1, duration=2),
+                        Task(id=1, machine=2, duration=2),
+                        Task(id=2, machine=3, duration=2),
+                    ],
+                ),
+                1: Job(
+                    id=1,
+                    tasks=[
+                        Task(id=0, machine=3, duration=2),
+                        Task(id=1, machine=2, duration=2),
+                        Task(id=2, machine=1, duration=2),
+                    ],
+                ),
+            },
+            "_fitted": False,
+        }
+
+        # ASSERT
+        assert output == expected
+
+    def test_horizonCorrectOutput(self, scheduler_example: Scheduler):
+        # OUTPUT
+        output = scheduler_example.horizon
+
+        # EXPECTED
+        expected = 12
+
+        # ASSERT
+        assert output == expected
+
+    def test_all_machinesCorrectOutput(self, scheduler_example: Scheduler):
+        # OUTPUT
+        output = scheduler_example.all_machines
+
+        # EXPECTED
+        expected = [1, 2, 3]
+
+        # ASSERT
+        assert output == expected
+
+    def test_get_resultsRaisesModelNotFitted(self, scheduler_example: Scheduler):
+        with pytest.raises(ModelNotFitted):
+            _ = scheduler_example.get_results()
+
+    def test_get_resultsCorrectOutput(
+        self, scheduler_example: Scheduler, results_example: pd.DataFrame
+    ):
+        # OUTPUT
+        output = scheduler_example.fit().get_results()
+
+        # EXPECTED
+        expected = results_example
+
+        # ASSERT
+        assert reduce(
+            lambda a, b: a and b,
+            [(expected[c] == output[c]).all() for c in output.columns],
+        )
